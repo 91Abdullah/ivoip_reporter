41c9fff1636df08f4ba8bd9fafab2ebe7f41070c
Abdullah Basit <abdullah.basit@hotmail.com>
2018-07-20 19:45:04 +0500

    Fixed recordings index

--- resources/views/recordings/show.blade.php
+++ resources/views/recordings/show.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-<div class="col-md-10 col-md-offset-1">
+<div class="col-md-12">
     <div class="panel panel-default">
         <div class="panel-body">    
             <h1>Call Recordings</h1>
